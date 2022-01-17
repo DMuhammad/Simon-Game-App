6b59ef1d13ba0b5d2754d2dc914e522528170052
Dzikri Muhammad <notfnd123@gmail.com>
2022-01-17 14:22:11 +0700

    add animation + sounds

--- game.js
+++ game.js
@@ -5,8 +5,11 @@ function nextSequence(){
     const randomNumber = Math.floor(Math.random() * 4);
     const randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
-    // console.log(randomChosenColour);
-}
+    
+    $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100);
+    const audio = new Audio(`sounds/${randomChosenColour}.mp3`);
+    audio.play();
+
+};
 
 nextSequence();
-console.log(gamePattern);
