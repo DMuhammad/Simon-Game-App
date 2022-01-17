14e4c1476e086a385783a16960af615938a8a469
Dzikri Muhammad <notfnd123@gmail.com>
2022-01-17 14:55:44 +0700

    add animation user click

--- game.js
+++ game.js
@@ -6,14 +6,26 @@ function nextSequence(){
     const randomNumber = Math.floor(Math.random() * 4);
     const randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
-    
+    playSound(randomChosenColour);
     $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100);
-    const audio = new Audio(`sounds/${randomChosenColour}.mp3`);
-    audio.play();
     $(".btn").click(function(){
         const userChosenColour = this.id;
+        playSound(userChosenColour);
+        animatePress(userChosenColour);
         userClickedPattern.push(userChosenColour);
     })
 };
+
+function playSound(name){
+    const audio = new Audio(`sounds/${name}.mp3`);
+    audio.play();
+}
+
+function animatePress(currentColour){
+    $(`.${currentColour}`).addClass("pressed");
+    setTimeout(function(){
+        $(`.${currentColour}`).removeClass("pressed");
+    },100);
+}
 
 nextSequence();
